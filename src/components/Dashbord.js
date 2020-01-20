b031f966efdafb00c611c812556714d0e773c1a5
taras <tviyatyk@gmail.com>
2020-01-20 23:29:37 +0200

    Add search feature

--- src/components/Dashbord.js
+++ src/components/Dashbord.js
@@ -5,26 +5,44 @@ import ReactPaginate from 'react-paginate';
 const Dashboard = props => {
   const [employees, setEmployees] = useState();
   const [meta, setMeta] = useState();
+  const [searchValue, setSearchValue] = useState('');
 
   useEffect(() => {
     getEmployees()
       .then(resp => {
         setEmployees(resp.data.employees);
         setMeta(resp.data.meta);
-        console.log(resp);
       })
   }, []);
 
   const onPageChangeHandler = page => {
-    getEmployees(page.selected + 1)
+    getEmployees(page.selected + 1, 10, searchValue)
       .then(resp => {
         setEmployees(resp.data.employees);
         setMeta(resp.data.meta);
       })
   }
 
+  const searchChangeHandler = event => {
+    setSearchValue(event.target.value);
+  }
+
+  const searchSubmitHandler = event => {
+    event.preventDefault();
+    getEmployees(1, 10, searchValue)
+      .then(resp => {
+        setEmployees(resp.data.employees);
+        setMeta(resp.data.meta);
+        console.log(resp)
+      })
+  }
+
   return (
     <div className="dashboard">
+      <form onSubmit={searchSubmitHandler}>
+        <input value={searchValue} onChange={searchChangeHandler} type='text' />
+        <button type='submit'>Search</button>
+      </form>
       <table>
         <thead>
           <tr>
@@ -46,20 +64,20 @@ const Dashboard = props => {
         </tbody>
       </table>
       <ReactPaginate
-          previousLabel={'previous'}
-          nextLabel={'next'}
-          breakLabel={'...'}
-          breakClassName={'break-me'}
-          pageCount={meta?.totalPages}
-          marginPagesDisplayed={2}
-          pageRangeDisplayed={5}
-          initialSelected={meta?.currentPage}
-          forcePage={meta?.currentPage}
-          onPageChange={onPageChangeHandler}
-          containerClassName={'pagination'}
-          subContainerClassName={'pages pagination'}
-          activeClassName={'active'}
-        />
+        previousLabel={'previous'}
+        nextLabel={'next'}
+        breakLabel={'...'}
+        breakClassName={'break-me'}
+        pageCount={meta?.totalPages}
+        marginPagesDisplayed={2}
+        pageRangeDisplayed={5}
+        initialSelected={meta?.currentPage}
+        forcePage={meta?.currentPage}
+        onPageChange={onPageChangeHandler}
+        containerClassName={'pagination'}
+        subContainerClassName={'pages pagination'}
+        activeClassName={'active'}
+      />
     </div>
   );
 }
